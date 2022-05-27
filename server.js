e4d790d2701ea5fdac004526867eb08e8748c25c
Arjun Tanwar <arjunthakur900@gmail.com>
2022-05-27 18:20:43 +0530

    swaggerui

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 app.set('trust proxy', 1)
 
@@ -25,6 +28,11 @@ app.use(rateLimiter({
 app.use(helmet())
 app.use(cors())
 app.use(xss())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Jobs api</h1><a href = "/api-docs">Documentations</a>`)
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authMidWare, jobsRouter)
